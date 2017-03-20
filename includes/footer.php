1d5d81bf3397f44ee0bb178707fd8c0a61ca888e
GuimoO <guiguismoo@gmail.com>
2017-03-20 13:42:56 +0100

    rajout meta header.php et déplacement script en footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,2 +1,6 @@
-</body>
+<!--Jquery -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+
+	</body>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -2,9 +2,11 @@
 <html lang="fr">
 <head>
 	<meta charset="UTF-8">
+	<meta name="author" content="Lucie Colon, Auzanet Charly, Lemarinier Jérémy, Hery, Guillaume">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 	<title>MatchMyPics - WCS</title>
-	<!--Jquery -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	
 	<!-- Bootstrap -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	<!-- Boostrap Js -->
