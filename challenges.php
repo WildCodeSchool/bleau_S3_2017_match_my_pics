1e2417c8bc9d2ca3bc2852464e5c7c31e525f6e5
CharlyAuzanet <charly.auzanet@gmail.com>
2017-03-21 15:39:10 +0100

    travail en cours

---
+++ challenges.php
@@ -0,0 +1,27 @@
+<?php 
+	include 'includes/header.php';
+	include 'includes/header_return.php';
+?>
+<link rel="stylesheet" type="text/css" href="assets/css/challenges.css"/>
+<div class="container liste_challenges">
+	<div class="row">
+		<div class="col-xs-offset-1 col-xs-10 container_chall">
+			<div class="row a">
+				<div class="col-xs-8 challengediv">
+						<h2>Challenge 1</h2>
+				</div>
+				<div class="col-xs-4">
+					<div class="row">
+						<div class="col-xs-12 pts_div">
+							<h3>1250</h3>
+						</div>
+						<div class="col-xs-12 diff_div">
+							<h3>Easy</h3>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
