cdfb5b9d808356fb425cc7ffe2dac7f305022299
JIM2022 <jaymail04@gmail.com>
2017-03-22 16:33:47 +0100

    challenge_x Test

--- challenge_x.php
+++ challenge_x.php
@@ -3,40 +3,44 @@ include 'includes/header.php';
 include 'includes/header_return.php';
 ?>
 
-<div class="container challenge_x">
+    <div class="container challenge_x">
 
-    <!--Container Title Challenge_x-->
-    <div class="row title_challenge_x">
-        <div class="col-xs-offset-3">
-            <p>Challenge_x</p>    
+        <!--Container Title Challenge_x-->
+        <div class="row title_challenge_x">
+            <div class="col-xs-offset-3">
+                <p>Challenge_x</p>    
+            </div>
+        </div>
+
+        <!--Container Pics-->
+        <div class="row pics">
+            <div class="col-xs-offset-3 col-xs-9 divpics">
+                <img src="assets/img/licorne.jpg" id="img" alt="cerf">
+            </div>
+        </div>
+
+        <!--Container teaser_indices-->    
+        <div class="row teaser_indices">
+            <div class="col-xs-offset-2 col-xs-10">
+                <p>------->petit indice--------------> </p>  
+            </div>
+        </div>
+
+        <!--Btn indices + add_soluce-->
+        <div class="row btn">
+            <div class="col-xs-offset-1 col-xs-4 ">
+                <a href="indice.php"><input id="indices" type="submit" name="Indices" value="indices"/>
+                </a>
+            </div>
+            <div class="col-xs-offset-2 col-xs-4 ">
+                      <a href="add_your_pics.php"><input id="add_soluce" type="submit" name="add_soluce" value="Solution"/>
+                      </a>
+            </div>
+            <div class="col-xs-offset-4 col-xs-4">
+                      <a href="#"><input id="abandonment" type="submit" name="abandonment" value="Abandon"/>
+                      </a>
+            </div>
         </div>
     </div>
-    
-    <!--Container Pics-->
-    <div class="row pics">
-        <div class="col-xs-offset-3 col-xs-9 divpics">
-            <img src="assets/img/cerf1.png" id="img" alt="cerf">
-        </div>
-    </div>
-
-    <!--Container teaser_indices-->    
-    <div class="row teaser_indices">
-        <div class="col-xs-offset-2 col-xs-10">
-            <p>------->petit indice--------------> </p>  
-        </div>
-    </div>
-              
-    <!--Btn indices + add_soluces-->
-    <div class="row btn">
-        <div class="col-xs-offset-2 col-xs-4 ">
-            <a href="#"><input id="indices" type="submit" name="indices" value="indices"/></a>
-        </div>
-        <div class="col-xs-offset-2 col-xs-4 ">
-                  <a href="#"><input id="add_soluces" type="submit" name="add_soluces" value="add_soluces"/></a>
-        </div>
-    </div>
-
-
-</div>
 
 <?php include 'includes/footer.php' ?>
