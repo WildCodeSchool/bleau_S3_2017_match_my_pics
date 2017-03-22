fa4374f4ea5d0b186b963f189c300e03fdd7083a
JIM2022 <jaymail04@gmail.com>
2017-03-22 12:10:02 +0100

    responsive_img

--- challenge_x.php
+++ challenge_x.php
@@ -5,35 +5,32 @@ include 'includes/header_return.php';
 
 <div class="container challenge_x">
 
-<!--Container Title Challenge_x-->
+    <!--Container Title Challenge_x-->
     <div class="row title_challenge_x">
         <div class="col-xs-offset-3 col-xs-9">
-            <h2>Challenge_x</h2>    
+            <p>Challenge_x</p>    
         </div>
     </div>
     
-<!--Container Pics-->
-   
+    <!--Container Pics-->
     <div class="row pics">
-        <div class="col-xs-offset-3 col-xs-9">
-            <img src="/assets/img/logo_orange.png" alt="licorne">
+        <div class="col-xs-offset-3 col-xs-9 divpics">
+            <img src="assets/img/licorne.jpg" id="img" alt="cerf">
         </div>
     </div>
 
-<!--Container teaser_indices-->    
+    <!--Container teaser_indices-->    
     <div class="row teaser_indices">
         <div class="col-xs-offset-2 col-xs-10">
             <p>------->petit indice--------------> </p>  
         </div>
     </div>
               
-<!--Btn indices + add_soluces-->
-   
+    <!--Btn indices + add_soluces-->
     <div class="row btn">
         <div class="col-xs-offset-2 col-xs-4 ">
             <a href="#"><input id="indices" type="submit" name="indices" value="indices"/></a>
         </div>
-
         <div class="col-xs-offset-2 col-xs-4 ">
                   <a href="#"><input id="add_soluces" type="submit" name="add_soluces" value="add_soluces"/></a>
         </div>
