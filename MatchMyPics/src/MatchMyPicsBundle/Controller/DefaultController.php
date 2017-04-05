ebc653a281bc8b12aa93a0f0ccea9b52de11546c
JIM2022 <jaymail04@gmail.com>
2017-04-05 11:41:12 +0200

    view_Admin ok

--- MatchMyPics/src/MatchMyPicsBundle/Controller/DefaultController.php
+++ MatchMyPics/src/MatchMyPicsBundle/Controller/DefaultController.php
@@ -20,20 +20,30 @@ class DefaultController extends Controller
     {
         return $this->render('@MatchMyPics/user/home.html.twig');
     }
+
     public function challengesAction()
     {
         return $this->render('@MatchMyPics/user/challenges.html.twig');
     }
+
     public function challenge_xAction()
     {
         return $this->render('@MatchMyPics/user/challenge_x.html.twig');
     }
+
     public function indiceAction()
     {
         return $this->render('@MatchMyPics/user/indice.html.twig');
     }
+
     public function solutionAction()
     {
         return $this->render('@MatchMyPics/user/solution.html.twig');
     }
+
+    public function sommaire_AdminAction()
+    {
+        return $this->render('@MatchMyPics/admin/sommaire_Admin.html.twig');
+    }
 }
+
